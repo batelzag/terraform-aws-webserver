31e817d852be89691838527703fef9558c4b5102
batelzag <88581982+batelzag@users.noreply.github.com>
2021-11-22 23:27:42 +0200

    Update main.tf

--- setups/instances/main.tf
+++ setups/instances/main.tf
@@ -1,24 +1,24 @@
 # module "webserver_vpc" {
-# 	source = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/vpc"
+# 	source = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/vpc"
 #   	version = "1.0.0"
 # }
 data "terraform_remote_state" "webserver_vpc" {
 	backend = "remote"
 	config = {
-		organization = "Batel-OpsSchool"
+		organization = "Batel-OpsSchool1"
 		workspaces = {
       		name = "Network"
 		}
     }
 }
 module "webserver_web" {
-  	source  = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/web"
+  	source  = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/web"
   	version = "1.0.0"
   	vpc_id = data.terraform_remote_state.webserver_vpc.outputs.vpc_id
 	public_subnets = "${data.terraform_remote_state.webserver_vpc.outputs.public_subnets[*]}"
 }
 module "webserver_db" {
-  	source  = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/db"
+  	source  = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/db"
   	version = "1.0.0"	
 	vpc_id = data.terraform_remote_state.webserver_vpc.outputs.vpc_id
 	vpc_cidr = data.terraform_remote_state.webserver_vpc.outputs.vpc_cidr
