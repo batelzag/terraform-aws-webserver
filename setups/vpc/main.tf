b812d7d894761f842a886e77dafa36db52d5652b
batelzag <88581982+batelzag@users.noreply.github.com>
2021-11-22 23:28:09 +0200

    Update main.tf

--- setups/vpc/main.tf
+++ setups/vpc/main.tf
@@ -1,15 +1,15 @@
 module "webserver_vpc" {
-	source = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/vpc"
+	source = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/vpc"
   	version = "1.0.0"
 }
 # module "test_WEB" {
-#   	source  = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/web"
+#   	source  = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/web"
 #   	version = "1.0.0"
 #   	vpc_id = data.terraform_remote_state.webserver_vpc.vpc_id
 # 	public_subnets = "${data.terraform_remote_state.webserver_vpc.public_subnets[*]}"
 # }
 # module "test_DB" {
-#   	source  = "app.terraform.io/Batel-OpsSchool/webserver/aws//modules/db"
+#   	source  = "app.terraform.io/Batel-OpsSchool1/webserver/aws//modules/db"
 #   	version = "1.0.0"	
 # 	vpc_id = data.terraform_remote_state.webserver_vpc.vpc_id
 # 	vpc_cidr = data.terraform_remote_state.webserver_vpc.vpc_cidr
